cb735198f6f00d14a2be30ef7099fa6f83c67ed1
Acácio Vasconcelos Telechi <acaciotelechi@gmail.com>
2022-01-24 20:57:48 -0300

    update Filtro

--- src/views/Obras/ObrasPage.js
+++ src/views/Obras/ObrasPage.js
@@ -11,23 +11,16 @@ import db from "../../serv/db.json";
 
 import "../../styles/css/style.css";
 
-// const filtros = [
-//   { titulo: "Tamanho", dropdown: [{"id":"999", "nome": "Range selector com o tamanho AAxBB"}] },
-//   { titulo: "Estilo", dropdown: estilos },
-//   { titulo: "Categorias", dropdown: categorias },
-//   { titulo: "Cor", dropdown: cores },
-//   {
-//     titulo: "Faixa de preço",
-//     dropdown: [{"id":"666", "nome": "Range selector com o tamanho AAxBB"}],
-//   },
-// ];
 
 function ObrasPage() {
   const obras = db.obras;
 
   return (
     <div>
-      <Filtro />
+      <div className="filtros" style={{"height": '80px'}}>
+        FILTROS
+        <Filtro />
+      </div>
       <div className="cardContainer">
         {obras.map((obra) => (
           <CardObra nome={obra.nome} img={obra.img} />
--- src/views/Obras/filtro/Filtro.js
+++ src/views/Obras/filtro/Filtro.js
@@ -1,23 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import FiltroItem from "./FiltroItem";
 
-import estilos from "../../../utils/estilos.json"
-import cores from "../../../utils/cores.json"
+import estilos from "../../../utils/estilos.json";
+import cores from "../../../utils/cores.json";
 
 import "../../../styles/css/style.css";
 
 function Filtro() {
   const [isOpen, setIsOpen] = useState(false);
+  const [scrollPos, setScrollPos] = useState([0, 0]);
+  const [isHide, setIsHide] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+
+    if (scrollPos[0] > scrollPos[1]) {
+      setIsHide(true);
+    } else if (scrollPos[0] < scrollPos[1]) {
+      setIsHide(false);
+    }
+    return () => window.removeEventListener("scroll", listenToScroll);
+  }, [scrollPos]);
+
+  const listenToScroll = () => {
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    setScrollPos([scrollPos[1], winScroll]);
+  };
+
+  console.log(scrollPos);
+
   return (
     <div className="container-fluid p-1">
       {isOpen && (
         <div className="filtro-content p-1">
-          <FiltroItem titulo= "Estilos" obj={estilos}/>
-          <FiltroItem titulo= "Cores" obj={cores}/>
+          <FiltroItem titulo="Estilos" obj={estilos} />
+          <FiltroItem titulo="Cores" obj={cores} />
         </div>
       )}
-      <div className="filtro px-1" onClick={() => setIsOpen(!isOpen)}>
+      <div
+        className="filtro px-1"
+        onClick={() => setIsOpen(!isOpen)}
+        style={{
+          display: isHide ? "block" : "none",
+        }}
+      >
         Filtro
       </div>
     </div>
