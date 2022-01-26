1b7192e5e13ebecd74b0809cdb6354cc10614e58
Acácio Vasconcelos Telechi <acaciotelechi@gmail.com>
2022-01-26 16:39:27 -0300

    Update MiniCardPerfil.js

--- src/components/perfil/MiniCardPerfil.js
+++ src/components/perfil/MiniCardPerfil.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import SeguirButton from "./SeguirButton";
+import SeguirButton from "../botoes/SeguirButton";
 
 function MiniCardPerfil(props) {
   const { perfil } = props;
