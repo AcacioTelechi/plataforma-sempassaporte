aaa6740e1d3ff53658ef029bdf7a9026407ca8a9
Acácio Vasconcelos Telechi <acaciotelechi@gmail.com>
2022-01-27 00:30:14 -0300

    Update Preco.js

--- src/components/preco/Preco.js
+++ src/components/preco/Preco.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React from "react";
 
 import "../../styles/css/style.css";
 
 function Preco(props) {
-    const { obra } = props;
-    if (obra.disp === "true") {
-        return (
-            <div className="text-price my-2">
-                   R$ {obra.preco}
-            </div>
-        );
-    } else {
-        return (
-            <div className="text-secondary my-2" style={{textAlign: "left"}}>
-                    Não disponível para venda
-            </div>
-        );
-    }
+  const { obra } = props;
+  if (obra.disp === "true") {
+    return <div className="text-price my-2">R$ {obra.preco}</div>;
+  } else {
+    return (
+      <div className="text-secondary my-2" style={{ textAlign: "left" }}>
+        Não disponível para venda
+      </div>
+    );
+  }
 }
-export default Preco
+export default Preco;
